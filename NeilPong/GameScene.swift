8e4780d918edacc6a4bed69a38cd53bc194cea7b
Neil Houselander <neilhouselander@sky.com>
2017-08-10 11:43:29 +0100

    added bat & ball to initial scene and added relevant physics bodies

--- NeilPong/GameScene.swift
+++ NeilPong/GameScene.swift
@@ -11,18 +11,22 @@ import GameplayKit
 
 
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //globals for this view
     let playerOneScoreLabel = SKLabelNode(fontNamed: "LLPixel")
     let playerTwoScoreLabel = SKLabelNode(fontNamed: "LLPixel")
+    
+    let ball = SKSpriteNode(imageNamed: "ball")
+    let playerBat = SKSpriteNode(imageNamed: "myBat")
+    let enemyBat = SKSpriteNode(imageNamed: "enemyBat")
     
     //sound effects -declare here to avoid lag
     let hitBatSound = SKAction.playSoundFileNamed("pongBlipSound.wav", waitForCompletion: false)
     
     //physics
     struct PhysicsCategories{
-        static let None: UInt32 = 0
+        static let None: UInt32 = 0 //0
         static let Bat: UInt32 = 0b1 //1
         static let Ball: UInt32 = 0b10 //2
         static let Border:UInt32 = 0b100 //3
@@ -60,8 +64,53 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        //set up scene
+        self.physicsWorld.contactDelegate = self
+        
+        //set up background & physics border
         self.backgroundColor = SKColor.black
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
+        //add the assets
+        ball.setScale(1.0)
+        ball.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        ball.zPosition = 1
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2)
+        ball.physicsBody!.affectedByGravity = false
+        ball.physicsBody!.friction = 0
+        ball.physicsBody!.restitution = 1
+        ball.physicsBody!.categoryBitMask = PhysicsCategories.Ball
+        ball.physicsBody!.collisionBitMask = PhysicsCategories.Bat
+        ball.physicsBody!.contactTestBitMask = PhysicsCategories.Bat
+        self.addChild(ball)
+        
+        playerBat.setScale(1.0)
+        playerBat.position = CGPoint(x: self.size.width/2, y: self.size.height*0.10)
+        playerBat.zPosition = 1
+        playerBat.physicsBody = SKPhysicsBody(rectangleOf: playerBat.size)
+        playerBat.physicsBody?.affectedByGravity = false
+        playerBat.physicsBody?.friction = 0
+        playerBat.physicsBody?.restitution = 1
+        playerBat.physicsBody?.categoryBitMask = PhysicsCategories.Bat
+        playerBat.physicsBody?.collisionBitMask = PhysicsCategories.Ball
+        playerBat.physicsBody?.contactTestBitMask = PhysicsCategories.Ball
+        self.addChild(playerBat)
+        
+        enemyBat.setScale(1.0)
+        enemyBat.position = CGPoint(x: self.size.width/2, y: self.size.height*0.90)
+        enemyBat.zPosition = 1
+        enemyBat.physicsBody = SKPhysicsBody(rectangleOf: enemyBat.size)
+        enemyBat.physicsBody?.affectedByGravity = false
+        enemyBat.physicsBody?.friction = 0
+        enemyBat.physicsBody?.restitution = 1
+        enemyBat.physicsBody?.categoryBitMask = PhysicsCategories.Bat
+        enemyBat.physicsBody?.collisionBitMask = PhysicsCategories.Ball
+        enemyBat.physicsBody?.contactTestBitMask = PhysicsCategories.Ball
+        self.addChild(enemyBat)
         
         
         
